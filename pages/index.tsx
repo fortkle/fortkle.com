afb4e2bf879614071cc63f0949e99131027d27ec
fortkle <fortkle@gmail.com>
2020-05-17 07:13:38 +0900

    remove unneeded files

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>FRTKL</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <p>this is top page.</p>
+    </div>
+  )
+}
