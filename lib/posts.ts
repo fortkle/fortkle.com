7bfdac3bb14025cc5c4cba67822106610a2d9852
fortkle <fortkle@gmail.com>
2020-05-17 18:37:10 +0900

    add post page

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,5 +1,9 @@
 import parseISO from 'date-fns/parseISO'
 import formatISO from 'date-fns/formatISO'
+import matter from 'gray-matter'
+import remark from 'remark'
+import html from 'remark-html'
+import highlight from 'remark-highlight.js'
 
 const url = 'https://api.esa.io/v1/teams/fortkle'
 
@@ -32,4 +36,54 @@ export async function getSortedPostsData() {
       return -1
     }
   })
+}
+
+export async function getRecentPostIds() {
+  const params = { sort: 'created', q: 'category:posts', per_page: '1' };
+  const qs = new URLSearchParams(params);
+
+  const response = await fetch(
+    `${url}/posts?${qs}`,
+    { headers: { 'Authorization': `Bearer ${process.env.ESA_ACCESS_TOKEN}` } }
+  )
+  const json = await response.json()
+
+  return json.posts.map(post => {
+    return {
+      params: {
+        id: post.number.toString(),
+      }
+    }
+  })
+}
+
+export async function getPostData(id: string) {
+  const response = await fetch(
+    `${url}/posts/${id}?`,
+    { headers: { 'Authorization': `Bearer ${process.env.ESA_ACCESS_TOKEN}` } }
+  )
+  if (response.status !== 200) {
+    return false
+  }
+  const json = await response.json()
+
+  const categoryDate = json.category
+    .replace(/^posts\//, '')
+    .replace(/\//g, '-')
+
+
+  const matterResult = matter(json.body_md)
+  const processedContent = await remark()
+    .use(html)
+    .use(highlight)
+    .process(matterResult.content)
+  const contentHtml = processedContent.toString()
+
+  return {
+    id: json.number,
+    date: formatISO(parseISO(categoryDate)),
+    title: json.name,
+    contentHtml: contentHtml,
+    contentHtml2: json.body_html,
+  }
 }
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,9 +1,49 @@
+import Head from 'next/head'
+import Error from 'next/error'
 import Layout from '../../components/layout'
+import Date from '../../components/date'
+import Profile from '../../components/profile'
+import { getRecentPostIds, getPostData } from '../../lib/posts'
 
-export default function Post() {
+export default function Post({ postData }) {
+  if (!postData) {
+    return <Error statusCode={404} />
+  }
+
   return (
     <Layout>
-      <p>this is post page.</p>
+      <Head>
+        <title>{postData.title}</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.0.3/styles/darcula.min.css" />
+      </Head>
+      <article>
+        <h1>{postData.title}</h1>
+        <div>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+      <section>
+        <h5>Written By Fukuaki TAKANO</h5>
+        <Profile />
+      </section>
     </Layout>
   )
+}
+
+export async function getStaticPaths() {
+  const paths = await getRecentPostIds()
+  return {
+    paths,
+    fallback: true,
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
 }
