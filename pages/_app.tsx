3cef569ddb89e32df6cd74be84a35ea18459d56f
fortkle <fortkle@gmail.com>
2020-08-08 00:18:08 +0900

    setting for pageview

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
 import { AppProps } from 'next/app'
 import '../styles/global.css'
+import * as gtag from '../lib/gtags'
+import { useEffect } from 'react'
+import { Router } from 'next/router'
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (!gtag.existsGaTrackingId) {
+      return
+    }
+
+    const handleRouteChange = (path) => {
+      gtag.pageview(path)
+    }
+
+    Router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [])
+
   return <Component {...pageProps} />
 }
